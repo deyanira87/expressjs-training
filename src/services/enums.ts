151a69980425a3e2948cf91c61b1a977f3dea5c9
Dayli Suarez <dayli8791@gmail.com>
2024-02-21 16:35:17 -0500

    [ADD] Testing Express.js

---
+++ src/services/enums.ts
@@ -0,0 +1,14 @@
+export enum Visibility {
+  Great = 'great',
+  Good = 'good',
+  Ok = 'ok',
+  Poor = 'poor'
+}
+
+export enum Weather {
+  Sunny = 'sunny',
+  Rainy = 'rainy',
+  CLoudy = 'cloudy',
+  Windy = 'windy',
+  Stormy = 'stormy'
+}
---
+++ src/types.d.ts
@@ -0,0 +1,13 @@
+import { Weather, Visibility } from './services/enums'
+
+export interface DiaryEntry {
+  id: number
+  date: string
+  weather: Weather
+  visibility: Visibility
+  comment: string
+}
+
+// export type NonSensitiveInfoDiaryEntry = Pick<DiaryEntry, 'id' | 'date' | 'comment' | 'visibility'>
+export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, 'comment'>
+export type NewDiaryEntry = Omit<DiaryEntry, 'id'>
